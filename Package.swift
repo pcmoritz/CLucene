77479bbde1b3a0271c7b43c2d2b80659d3a6558f
Philipp Moritz <pcmoritz@gmail.com>
2018-06-23 18:55:20 -0700

    this is insane

--- Package.swift
+++ Package.swift
@@ -1,20 +1,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "CLucene",
-    pkgConfig: "liblucene++.pc",
-    providers: [
-        .brew(["curl"]),
-        .apt(["libcurl4-openssl-dev"])
-    ],
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "CLucene",
-            targets: ["CLucene"]
-        )
-    ],
-    targets: [
-        .target(name: "CLucene")
-    ]
+  name: "CLucene",
+  pkgConfig: "liblucene++.pc",
+  targets: [
+    .target(name: "CLucene")
+  ],
+  products: [
+    // Products define the executables and libraries produced by a package, and make them visible to other packages.
+    .library(
+      name: "CLucene",
+      targets: ["CLucene"]
+    )
+  ]
 )
