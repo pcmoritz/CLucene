80e8c9c765e5053c11d955d583155cf6ae9f00f8
Philipp Moritz <pcmoritz@gmail.com>
2018-06-23 18:35:40 -0700

    fix bug

--- Package.swift
+++ Package.swift
@@ -2,5 +2,19 @@ import PackageDescription
 
 let package = Package(
     name: "CLucene",
-    pkgConfig: "liblucene++.pc"
+    providers: [
+        .brew(["curl"]),
+        .apt(["libcurl4-openssl-dev"])
+    ],
+    pkgConfig: "liblucene++.pc",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "CLucene",
+            targets: ["CLucene"]
+        )
+    ],
+    targets: [
+        .target(name: "CLucene")
+    ]
 )
