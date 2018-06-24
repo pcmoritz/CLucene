aad0a281e1964d6cd108e52c888fcd7b791c855f
Philipp Moritz <pcmoritz@gmail.com>
2018-06-23 20:02:59 -0700

    update

--- shim.h
+++ shim.h
@@ -7,9 +7,9 @@ typedef struct LuceneContext LuceneContext;
 
 LuceneContext* LuceneCreateContext(const char* indexDirectory);
 
-// void buff_deinit( buff* _this );
-// buff* buff_init( int type, double amount, double max_amount, double start_angle, double relative_angle, double max_distance, double bleedthrough, bool permanent );
-// int buff_get_type( buff* _this );
+void LuceneIndexDocs(LuceneContext* context, const char* sourceDirectory);
+
+void LuceneDeleteContext(LuceneContext* context);
 
 #if __cplusplus
 }
